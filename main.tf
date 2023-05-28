3570ce17b9077ce8556f483e96828c408ee02315
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-28 15:46:45 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -29,15 +29,11 @@ resource "aws_eip" "ngw" {
   tags =merge(var.tags,{"Name"="${var.env}-ngw"})
 }
 
-#resource "aws_nat_gateway" "ngw" {
-#  allocation_id = aws_eip.example.id
-#  subnet_id     = aws_subnet.example.id
-#
-#  tags = {
-#    Name = "gw NAT"
-#  }
-#
-#  # To ensure proper ordering, it is recommended to add an explicit dependency
-#  # on the Internet Gateway for the VPC.
-#  depends_on = [aws_internet_gateway.example]
-#}
+resource "aws_nat_gateway" "ngw" {
+  count=length(lookup(lookup(var.subnets,"public",null ),"cidr_block",0))
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = module.subnets["public"].subnets_ids[count.index]
+
+  tags =merge(var.tags,{"Name"="${var.env}-ngw"})
+}
+
